c93e662b7833d4eb096eae2d44ca65ac0e734fac
Shamil Alisultanov <sh.alisultanov@maximatelecom.ru>
2019-05-15 23:27:50 +0300

    create components navbar and sidenav

---
+++ src/components/SideNav.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul class="sidenav app-sidenav open">
+    <li>
+      <a href="#" class="waves-effect waves-orange pointer">Счет</a>
+    </li>
+    <li>
+      <a href="#" class="waves-effect waves-orange pointer">История</a>
+    </li>
+    <li>
+      <a href="#" class="waves-effect waves-orange pointer">Планирование</a>
+    </li>
+    <li>
+      <a href="#" class="waves-effect waves-orange pointer">Новая запись</a>
+    </li>
+    <li>
+      <a href="#" class="waves-effect waves-orange pointer">Категории</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: "SideNav"
+  }
+</script>
+
+<style scoped>
+
+</style>
