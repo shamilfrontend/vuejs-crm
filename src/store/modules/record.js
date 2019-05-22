12a2dc9b33ef776d167a751a9aaeae6f0c7f4d55
Shamil Alisultanov <sh.alisultanov@maximatelecom.ru>
2019-05-23 00:25:17 +0300

    Расчет и вывод данных на страницу планирования

--- src/store/modules/record.js
+++ src/store/modules/record.js
@@ -10,6 +10,17 @@ export default {
   mutations: {
   },
   actions: {
+    async fetchRecords({dispatch, commit}) {
+      try {
+        const uid = await dispatch('getUid');
+        const sourceData = await firebase.database().ref(`users/${uid}/records`).once('value');
+        const records = (sourceData).val() || {};
+        return Object.keys(records).map(key => ({...records[key], id: key}));
+      } catch (e) {
+        commit('setError', e);
+        throw e;
+      }
+    },
     async createRecord({dispatch, commit}, record) {
       try {
         const uid = await dispatch('getUid');
--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -2,19 +2,36 @@
   <div>
     <div class="page-title">
       <h3>Планирование</h3>
-      <h4>12 212</h4>
+      <h4>{{bill | currency('RUB')}}</h4>
     </div>
 
-    <section>
-      <div>
+    <loader v-if="loading"/>
+
+    <p
+      v-else-if="!categories.length"
+      class="center"
+    >
+      Категорий пока нет
+      <router-link to="/categories">Добавить новую категорию</router-link>
+    </p>
+
+    <section
+      v-else
+    >
+      <div
+        v-for="item in categories"
+        :key="item.id"
+        class="record"
+      >
         <p>
-          <strong>Девушка:</strong>
-          12 122 из 14 0000
+          <strong>{{ item.title }}:</strong>
+          {{ item.spend | currency}} из {{ item.limit | currency}}
         </p>
         <div class="progress">
           <div
-            class="determinate green"
-            style="width:40%"
+            class="determinate"
+            :class="[item.progressColor]"
+            :style="{width: item.progressPercent + '%'}"
           ></div>
         </div>
       </div>
@@ -23,11 +40,61 @@
 </template>
 
 <script>
+  import {mapGetters} from 'vuex';
+
   export default {
-    name: "Planning"
+    name: "Planning",
+
+    data() {
+      return {
+        loading: true,
+        categories: [],
+      };
+    },
+
+    computed: {
+      ...mapGetters(['info']),
+      bill() {
+        return this.info.bill;
+      },
+    },
+
+    async mounted() {
+      const records = await this.$store.dispatch('fetchRecords');
+      const categories = await this.$store.dispatch('fetchCategories');
+
+      this.categories = categories
+        .map(cat => {
+          const spend = records
+            .filter(record => record.categoryId === cat.id)
+            .filter(record => record.type === 'outcome')
+            .reduce((total, record) => {
+              return total += +record.amount;
+            }, 0);
+
+          const percent = 100 * spend / cat.limit;
+          const progressPercent = percent > 100 ? 100 : percent;
+          const progressColor = percent < 60
+            ? 'green'
+            : percent < 100
+              ? 'yellow'
+              : 'red';
+
+          return {
+            ...cat,
+            progressPercent,
+            progressColor,
+            spend,
+          };
+        });
+
+      this.loading = false;
+    }
   }
 </script>
 
 <style scoped>
-
+.record {
+  margin-bottom: 30px;
+}
 </style>
